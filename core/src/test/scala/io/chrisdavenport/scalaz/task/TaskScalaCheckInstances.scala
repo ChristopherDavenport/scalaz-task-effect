63abee3f5c0b94fb07a638f0cf81b74ac3daa12d
Chris Davenport <chris@christopherdavenport.tech>
2018-05-11 15:10:06 -0400

    Apply Scalafmt to Repo

--- core/src/test/scala/io/chrisdavenport/scalaz/task/TaskScalaCheckInstances.scala
+++ core/src/test/scala/io/chrisdavenport/scalaz/task/TaskScalaCheckInstances.scala
@@ -1,20 +1,20 @@
 package io.chrisdavenport.scalaz.task
 
 import org.scalacheck._
-import scalaz.{\/-, -\/, \/}
+import scalaz.{-\/, \/, \/-}
 import scalaz.concurrent.Task
 import cats._
 import cats.implicits._
 
 object TaskScalaCheckInstances extends TaskScalaCheckInstances
 
-trait TaskScalaCheckInstances{
+trait TaskScalaCheckInstances {
 
   implicit def cogenTask[A]: Cogen[Task[A]] =
-      Cogen[Unit].contramap(_ => ())
+    Cogen[Unit].contramap(_ => ())
 
-  implicit def taskEq[A: Eq](implicit E: Eq[Throwable]): Eq[Task[A]] = 
-    new Eq[Task[A]]{
+  implicit def taskEq[A: Eq](implicit E: Eq[Throwable]): Eq[Task[A]] =
+    new Eq[Task[A]] {
       def eqv(x: Task[A], y: Task[A]): Boolean = {
         val xatt: Throwable \/ A = x.attempt.unsafePerformSync
         val yatt: Throwable \/ A = y.attempt.unsafePerformSync
@@ -23,7 +23,7 @@ trait TaskScalaCheckInstances{
           case (\/-(x), \/-(y)) => x === y
           case (-\/(xf), -\/(yf)) => xf === yf
           case _ => false
-        } 
+        }
       }
     }
 
